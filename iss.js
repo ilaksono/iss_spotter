d05f60c87759d26f1ba8c27fb9b40eacc15f18dd
ilaksono <ian.laksono@gmail.com>
2020-09-24 16:53:01 +0000

    added ip fetch promise function

--- iss.js
+++ iss.js
@@ -1,8 +1,27 @@
 const request = require('request');
+const ipify = 'https://api.ipify.org/?format=json'
 
 
 const fetchMyIP = (cb) => {
-
+  return new Promise((res, rej) => {
+    request(ipify, (err, response, body) => {
+      // console.log(body);
+      let obj = JSON.parse(body);
+      // console.log(obj.ip);
+      if (!err)
+        res(cb(err, obj.ip));
+      else
+        rej(cb(err, obj.ip));
+    })
+  })
 }
+// fetchMyIP('');
 
-module.exports = { fetchMyIP }
+// fetchMyIP('').then(data => {
+//   console.log(data);
+// }).catch(err => {
+//   console.log(err);
+//   throw new Error(err);
+// })
+
+module.exports = { fetchMyIP };
