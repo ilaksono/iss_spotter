60a39ed8c80ffa327bbd2791d11316eebae03ff0
ilaksono <ian.laksono@gmail.com>
2020-09-24 20:08:42 +0000

    linted

--- index2.js
+++ index2.js
@@ -5,13 +5,13 @@ const { nextISSTimesForMyLocation  } = require('./iss-prom');
 // .then(body => fetchISSFlyOverTimes(body))
 // .then(body => print(body));
 const print = passes => {
-  for(let pass of passes) {
+  for (let pass of passes) {
     const date = new Date(0);
     date.setUTCSeconds(pass.risetime);
     console.log(`Next pass at ${date} for ${pass.duration} seconds!`);
   }
-}
+};
 
 nextISSTimesForMyLocation()
-.then(data => print(data))
-.catch(err => console.log('It didn\' work: ', err.message));
+  .then(data => print(data))
+  .catch(err => console.log('It didn\' work: ', err.message));
--- iss-prom.js
+++ iss-prom.js
@@ -6,21 +6,21 @@ const fetchMyIP = () => request(ipify);
 const fetchCoordsByIP = body => {
   const ip = JSON.parse(body).ip;
   return request('https://ipvigilante.com/' + ip);
-}
+};
 const fetchISSFlyOverTimes = body => {
   const coords = {'latitude': JSON.parse(body).data.latitude, 'longitude': JSON.parse(body).data.longitude};
-  return request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`)
-}
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`);
+};
 
 const nextISSTimesForMyLocation = () => {
   return fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(data => {
-    const {response} = JSON.parse(data); //{response} is same as obj.response.
-    return response;
-  })
-}
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(data => {
+      const {response} = JSON.parse(data); //{response} is same as obj.response.
+      return response;
+    });
+};
 
 
 module.exports = { nextISSTimesForMyLocation };
