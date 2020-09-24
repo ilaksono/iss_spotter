b5bed1f8a653c72aec934ad650bd7a6e8dc8e1e6
ilaksono <ian.laksono@gmail.com>
2020-09-24 18:56:01 +0000

    converted seconds to UTC datetime

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 // const { fetchMyIP } = require('./iss-prom');
-const { fetchMyIP, fetchCoordsByIP } = require('./iss-cb');
-const {IP} = require('./constants');
+const { nextISSTimesForMyLocation } = require('./iss-cb');
+const {obj} = require('./constants');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP(obj.IP, (error, data) => {
+//   // console.log(err, data)
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+
+
+// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, data) => {
+//   // console.log(err, data)
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+nextISSTimesForMyLocation((error, time) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
-  console.log('It worked! Returned IP:' , ip);
+  print(time);
+  
 });
 
-fetchCoordsByIP(IP, (error, data) => {
-  // console.log(err, data)
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+const print = passes => {
+  for(let pass of passes) {
+    const date = new Date(0);
+    date.setUTCSeconds(pass.risetime);
+    console.log(`Next pass at ${date} for ${pass.duration} seconds!`);
   }
-  console.log(data);
-});
+}
--- iss-cb.js
+++ iss-cb.js
@@ -1,6 +1,6 @@
 const request = require('request');
 const ipify = 'https://api.ipify.org/?format=json';
-const vigil = 'https://ipvigilante.com/'
+const vigil = 'https://ipvigilante.com/';
 
 const fetchMyIP = (cb) => {
   request(ipify, (err, response, body) => {
@@ -11,8 +11,7 @@ const fetchMyIP = (cb) => {
     else if (response.statusCode !== 200) {
       const msg = `Status code: ${response.statusCode} when fetching IP. Reponse: ${body}`;
       return cb(Error(msg), null);
-    }
-    else {
+    } else {
       let obj = JSON.parse(body);
       return cb(null, obj.ip);
     }
@@ -21,23 +20,58 @@ const fetchMyIP = (cb) => {
 
 const fetchCoordsByIP = (ip, cb) => {
   request(vigil + ip, (err, resp, body) => {
+    // console.log(ip);
     if (err) return cb(err, null);
     else if (resp.statusCode !== 200) {
       const msg = `Status code: ${resp.statusCode} when fetching IP. Response: ${body}`;
       return cb(Error(msg), null);
-    }
-    else {
+    } else {
       let obj = JSON.parse(body);
-      // console.log(obj.data.latitude);
-      return cb(null, {"latitude": obj.data.latitude, "longitude": obj.data.longitude});
+      // console.log(obj);
+      return cb(null, { "latitude": obj.data.latitude, "longitude": obj.data.longitude });
     }
   });
-}
+};
+
+const fetchISSFlyOverTimes = (coords, cb) => {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (err, resp, body) => {
+    if (err) return cb(err, null);
+    else if (resp.statusCode !== 200) {
+      const msg = `Status code: ${resp.statusCode} when fetching IP. Response: ${body}`;
+      return cb(Error(msg), null);
+    } else {
+      let obj = JSON.parse(body);
+      // console.log(obj.response);
+      return cb(null, obj.response);
+    }
+  });
+};
+
+const nextISSTimesForMyLocation = (cb) => {
+  fetchMyIP((error, ip) => {
+    if (error)
+      return cb(error, null);
+
+    fetchCoordsByIP(ip, (error, coords) => {
+      if (error)
+        return cb(error, null);
+
+      fetchISSFlyOverTimes(coords, (error, passes) => {
+        if (error)
+          return cb(error, null);
+        cb(null, passes);
+      });
+    });
+  });
+};
+
 
 //http://api.open-notify.org/iss-pass.json?lat=LAT&lon=LON
 //example op: Next pass at Fri Jun 01 2021 13:01:35 GMT-0700 (Pacific Daylight Time) for 465 seconds!
 
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  fetchISSFlyOverTimes,
+  nextISSTimesForMyLocation
 };
